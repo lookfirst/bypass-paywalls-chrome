731b076323ac56814b63026d509b178a6acf511c
Jon Stevens <latchkey@gmail.com>
2018-06-25 11:23:56 +0700

    code style changes

--- background.js
+++ background.js
@@ -1,185 +1,188 @@
 'use strict';
 
 const defaultSites = {
-  'The Age': 'theage.com.au',
-  'Baltimore Sun': 'baltimoresun.com',
-  'Barron\'s': 'barrons.com',
-  'Bloomberg': 'bloomberg.com',
-  'Crain\'s Chicago Business': 'chicagobusiness.com',
-  'Chicago Tribune': 'chicagotribune.com',
-  'Corriere Della Sera': 'corriere.it',
-  'Daily Press': 'dailypress.com',
-  'Dagens Nyheter': 'dn.se',
-  'The Economist': 'economist.com',
-  'Financial Times': 'ft.com',
-  'Glassdoor': 'glassdoor.com',
-  'Haaretz': 'haaretz.co.il',
-  'Haaretz English': 'haaretz.com',
-  'Hartford Courant': 'courant.com',
-  'Harvard Business Review': 'hbr.org',
-  'Het Financieele Dagblad': 'fd.nl',
-  'Inc.com': 'inc.com',
-  'Le Temps': 'letemps.ch',
-  'Los Angeles Times': 'latimes.com',
-  'Medium': 'medium.com',
-  'Medscape': 'medscape.com',
-  'MIT Technology Review': 'technologyreview.com',
-  'Nikkei Asian Review': 'asia.nikkei.com',
-  'NRC': 'nrc.nl',
-  'The Boston Globe': 'bostonglobe.com',
-  'The Mercury News': 'mercurynews.com',
-  'The Morning Call': 'mcall.com',
-  'The Nation': 'thenation.com',
-  'The New York Times': 'nytimes.com',
-  'The New Yorker': 'newyorker.com',
-  'OrlandoSentinel': 'orlandosentinel.com',
-  'Quora': 'quora.com',
-  'SunSentinel': 'sun-sentinel.com',
-  'TheMarker': 'themarker.com',
-  'The Seattle Times': 'seattletimes.com',
-  'The Sydney Morning Herald': 'smh.com.au',
-  'The Washington Post': 'washingtonpost.com',
-  'The Wall Street Journal': 'wsj.com',
-  'Wired': 'wired.com'
+	'The Age': 'theage.com.au',
+	'Baltimore Sun': 'baltimoresun.com',
+	'Barron\'s': 'barrons.com',
+	'Bloomberg': 'bloomberg.com',
+	'Crain\'s Chicago Business': 'chicagobusiness.com',
+	'Chicago Tribune': 'chicagotribune.com',
+	'Corriere Della Sera': 'corriere.it',
+	'Daily Press': 'dailypress.com',
+	'Dagens Nyheter': 'dn.se',
+	'The Economist': 'economist.com',
+	'Financial Times': 'ft.com',
+	'Glassdoor': 'glassdoor.com',
+	'Haaretz': 'haaretz.co.il',
+	'Haaretz English': 'haaretz.com',
+	'Hartford Courant': 'courant.com',
+	'Harvard Business Review': 'hbr.org',
+	'Het Financieele Dagblad': 'fd.nl',
+	'Inc.com': 'inc.com',
+	'Le Temps': 'letemps.ch',
+	'Los Angeles Times': 'latimes.com',
+	'Medium': 'medium.com',
+	'Medscape': 'medscape.com',
+	'MIT Technology Review': 'technologyreview.com',
+	'Nikkei Asian Review': 'asia.nikkei.com',
+	'NRC': 'nrc.nl',
+	'The Boston Globe': 'bostonglobe.com',
+	'The Mercury News': 'mercurynews.com',
+	'The Morning Call': 'mcall.com',
+	'The Nation': 'thenation.com',
+	'The New York Times': 'nytimes.com',
+	'The New Yorker': 'newyorker.com',
+	'OrlandoSentinel': 'orlandosentinel.com',
+	'Quora': 'quora.com',
+	'SunSentinel': 'sun-sentinel.com',
+	'TheMarker': 'themarker.com',
+	'The Seattle Times': 'seattletimes.com',
+	'The Sydney Morning Herald': 'smh.com.au',
+	'The Washington Post': 'washingtonpost.com',
+	'The Wall Street Journal': 'wsj.com',
+	'Wired': 'wired.com'
 };
 
 const restrictions = {
-  'barrons.com': 'barrons.com/articles'
+	'barrons.com': 'barrons.com/articles'
 };
 
 // Don't remove cookies before page load
 const allow_cookies = [
-'asia.nikkei.com',
-'wsj.com',
-'ft.com',
-'letemps.ch',
-'fd.nl',
-'bostonglobe.com',
-'mercurynews.com',
-'theage.com.au',
-'economist.com',
-'bostonglobe.com'
+	'asia.nikkei.com',
+	'wsj.com',
+	'ft.com',
+	'letemps.ch',
+	'fd.nl',
+	'bostonglobe.com',
+	'mercurynews.com',
+	'theage.com.au',
+	'economist.com',
+	'bostonglobe.com'
 ];
 
 // Removes cookies after page load
 const remove_cookies = [
-'asia.nikkei.com',
-'bloomberg.com',
-'ft.com',
-'letemps.ch',
-'fd.nl',
-'bostonglobe.com',
-'mercurynews.com',
-'theage.com.au',
-'economist.com',
-'bostonglobe.com',
-'nytimes.com',
-'wired.com'
+	'asia.nikkei.com',
+	'bloomberg.com',
+	'ft.com',
+	'letemps.ch',
+	'fd.nl',
+	'bostonglobe.com',
+	'mercurynews.com',
+	'theage.com.au',
+	'economist.com',
+	'bostonglobe.com',
+	'nytimes.com',
+	'wired.com'
 ];
 
 function setDefaultOptions() {
-  chrome.storage.sync.set({
-    sites: defaultSites
-  }, function() {
-    chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
-  });
+	chrome.storage.sync.set({
+		sites: defaultSites
+	}, function () {
+		chrome.tabs.create({'url': 'chrome://extensions/?options=' + chrome.runtime.id});
+	});
 }
 
 
 const blockedRegexes = [
-/.+:\/\/.+\.tribdss\.com\//,
-/thenation\.com\/.+\/paywall-script\.php/
+	/.+:\/\/.+\.tribdss\.com\//,
+	/thenation\.com\/.+\/paywall-script\.php/
 ];
 
 let enabledSites = [];
 
 // Get the enabled sites
 chrome.storage.sync.get({
-  sites: {}
-}, function(items) {
-  enabledSites = Object.keys(items.sites).map(function(key) {
-    return items.sites[key];
-  });
+	sites: {}
+}, function (items) {
+	enabledSites = Object.keys(items.sites).map(function (key) {
+		return items.sites[key];
+	});
 });
-
 
 // Listen for changes to options
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  for (const key in changes) {
-    let storageChange = changes[key];
-    if (key === 'sites') {
-      let sites = storageChange.newValue;
-      enabledSites = Object.keys(sites).map(function(key) {
-        return sites[key];
-      });
-    }
-  }
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+	for (const key in changes) {
+		let storageChange = changes[key];
+		if (key === 'sites') {
+			let sites = storageChange.newValue;
+			enabledSites = Object.keys(sites).map(function (key) {
+				return sites[key];
+			});
+		}
+	}
 });
-
 
 // Set and show default options on install
 chrome.runtime.onInstalled.addListener(function (details) {
-  if (details.reason === 'install') {
-    setDefaultOptions();
-  } else if (details.reason === 'update') {
-    // User updated extension
-  }
+	if (details.reason === 'install') {
+		setDefaultOptions();
+	} else if (details.reason === 'update') {
+		// User updated extension
+	}
 });
 
 
-chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-  if (blockedRegexes.some(function(regex) { return regex.test(details.url); })) {
-    return { cancel: true };
-  }
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+	if (blockedRegexes.some(function (regex) {
+		return regex.test(details.url);
+	})) {
+		return {cancel: true};
+	}
 
-  let isEnabled = enabledSites.some(function(enabledSite) {
-    let useSite = details.url.indexOf("." + enabledSite) !== -1;
-    if (enabledSite in restrictions) {
-      return useSite && details.url.indexOf(restrictions[enabledSite]) !== -1;
-    }
+	let isEnabled = enabledSites.some(function (enabledSite) {
+		let useSite = details.url.indexOf("." + enabledSite) !== -1;
+		if (enabledSite in restrictions) {
+			return useSite && details.url.indexOf(restrictions[enabledSite]) !== -1;
+		}
 
-    return useSite;
-  });
+		return useSite;
+	});
 
-  if (!isEnabled) {
-    return;
-  }
+	if (!isEnabled) {
+		return;
+	}
 
-  let requestHeaders = details.requestHeaders;
+	let requestHeaders = details.requestHeaders;
 
-  // remove cookies before page load
-  requestHeaders = requestHeaders.filter(function(requestHeader) {
-    return (requestHeader.name !== 'Referer');
-  }).map(function(requestHeader) {
-    for (const siteIndex in allow_cookies) {
-      if (details.url.indexOf(allow_cookies[siteIndex]) !== -1) {
-        return requestHeader;
-      }
-    }
-    if (requestHeader.name === 'Cookie') {
-      requestHeader.value = '';
-    }
-    return requestHeader;
-  });
+	// remove cookies before page load
+	requestHeaders = requestHeaders.filter(function (requestHeader) {
+		return (requestHeader.name !== 'Referer');
+	}).map(function (requestHeader) {
+		for (const siteIndex in allow_cookies) {
+			if (details.url.indexOf(allow_cookies[siteIndex]) !== -1) {
+				return requestHeader;
+			}
+		}
+		if (requestHeader.name === 'Cookie') {
+			requestHeader.value = '';
+		}
+		return requestHeader;
+	});
 
-  return { requestHeaders: requestHeaders };
+	return {requestHeaders: requestHeaders};
 }, {
-  urls: ['<all_urls>']
+	urls: ['<all_urls>']
 }, ['blocking', 'requestHeaders']);
 
 // remove cookies after page load
-chrome.webRequest.onCompleted.addListener(function(details) {
-  for (var domainIndex in remove_cookies) {
-    var domainVar = remove_cookies[domainIndex];
-    if (!enabledSites.includes(domainVar) || details.url.indexOf(domainVar) === -1) {
-      continue; // don't remove cookies
-    }
-    chrome.cookies.getAll({domain: domainVar}, function(cookies) {
-      for (let i=0; i<cookies.length; i++) {
-        chrome.cookies.remove({url: cookies[i].secure ? "https://" : "http://" + cookies[i].domain + cookies[i].path, name: cookies[i].name});
-      }
-    });
-  }
+chrome.webRequest.onCompleted.addListener(function (details) {
+	for (const domainIndex in remove_cookies) {
+		const domainVar = remove_cookies[domainIndex];
+		if (!enabledSites.includes(domainVar) || details.url.indexOf(domainVar) === -1) {
+			continue; // don't remove cookies
+		}
+		chrome.cookies.getAll({domain: domainVar}, function (cookies) {
+			for (let i = 0; i < cookies.length; i++) {
+				chrome.cookies.remove({
+					url: cookies[i].secure ? "https://" : "http://" + cookies[i].domain + cookies[i].path,
+					name: cookies[i].name
+				});
+			}
+		});
+	}
 }, {
-  urls: ["<all_urls>"]
+	urls: ['<all_urls>']
 });
--- options.js
+++ options.js
@@ -1,110 +1,110 @@
 const defaultSites = {
-  'The Age': 'theage.com.au',
-  'Baltimore Sun': 'baltimoresun.com',
-  'Barron\'s': 'barrons.com',
-  'Bloomberg': 'bloomberg.com',
-  'Crain\'s Chicago Business': 'chicagobusiness.com',
-  'Chicago Tribune': 'chicagotribune.com',
-  'Corriere Della Sera': 'corriere.it',
-  'Daily Press': 'dailypress.com',
-  'Dagens Nyheter': 'dn.se',
-  'The Economist': 'economist.com',
-  'Financial Times': 'ft.com',
-  'Glassdoor': 'glassdoor.com',
-  'Haaretz': 'haaretz.co.il',
-  'Haaretz English': 'haaretz.com',
-  'Hartford Courant': 'courant.com',
-  'Harvard Business Review': 'hbr.org',
-  'Het Financieele Dagblad': 'fd.nl',
-  'Inc.com': 'inc.com',
-  'Le Temps': 'letemps.ch',
-  'Los Angeles Times': 'latimes.com',
-  'Medium': 'medium.com',
-  'Medscape': 'medscape.com',
-  'MIT Technology Review': 'technologyreview.com',
-  'Nikkei Asian Review': 'asia.nikkei.com',
-  'NRC': 'nrc.nl',
-  'The Boston Globe': 'bostonglobe.com',
-  'The Mercury News': 'mercurynews.com',
-  'The Morning Call': 'mcall.com',
-  'The Nation': 'thenation.com',
-  'The New York Times': 'nytimes.com',
-  'The New Yorker': 'newyorker.com',
-  'OrlandoSentinel': 'orlandosentinel.com',
-  'Quora': 'quora.com',
-  'SunSentinel': 'sun-sentinel.com',
-  'TheMarker': 'themarker.com',
-  'The Seattle Times': 'seattletimes.com',
-  'The Sydney Morning Herald': 'smh.com.au',
-  'The Washington Post': 'washingtonpost.com',
-  'The Wall Street Journal': 'wsj.com',
-  'Wired': 'wired.com'
+	'The Age': 'theage.com.au',
+	'Baltimore Sun': 'baltimoresun.com',
+	'Barron\'s': 'barrons.com',
+	'Bloomberg': 'bloomberg.com',
+	'Crain\'s Chicago Business': 'chicagobusiness.com',
+	'Chicago Tribune': 'chicagotribune.com',
+	'Corriere Della Sera': 'corriere.it',
+	'Daily Press': 'dailypress.com',
+	'Dagens Nyheter': 'dn.se',
+	'The Economist': 'economist.com',
+	'Financial Times': 'ft.com',
+	'Glassdoor': 'glassdoor.com',
+	'Haaretz': 'haaretz.co.il',
+	'Haaretz English': 'haaretz.com',
+	'Hartford Courant': 'courant.com',
+	'Harvard Business Review': 'hbr.org',
+	'Het Financieele Dagblad': 'fd.nl',
+	'Inc.com': 'inc.com',
+	'Le Temps': 'letemps.ch',
+	'Los Angeles Times': 'latimes.com',
+	'Medium': 'medium.com',
+	'Medscape': 'medscape.com',
+	'MIT Technology Review': 'technologyreview.com',
+	'Nikkei Asian Review': 'asia.nikkei.com',
+	'NRC': 'nrc.nl',
+	'The Boston Globe': 'bostonglobe.com',
+	'The Mercury News': 'mercurynews.com',
+	'The Morning Call': 'mcall.com',
+	'The Nation': 'thenation.com',
+	'The New York Times': 'nytimes.com',
+	'The New Yorker': 'newyorker.com',
+	'OrlandoSentinel': 'orlandosentinel.com',
+	'Quora': 'quora.com',
+	'SunSentinel': 'sun-sentinel.com',
+	'TheMarker': 'themarker.com',
+	'The Seattle Times': 'seattletimes.com',
+	'The Sydney Morning Herald': 'smh.com.au',
+	'The Washington Post': 'washingtonpost.com',
+	'The Wall Street Journal': 'wsj.com',
+	'Wired': 'wired.com'
 };
 
 // Saves options to chrome.storage
 function save_options() {
-  let inputEls = document.querySelectorAll('#bypass_sites input');
+	let inputEls = document.querySelectorAll('#bypass_sites input');
 
-  let sites = Array.from(inputEls).reduce(function(memo, inputEl) {
-    if (inputEl.checked) {
-      memo[inputEl.dataset.key] = inputEl.dataset.value;
-    }
-    return memo;
-  }, {});
+	let sites = Array.from(inputEls).reduce(function (memo, inputEl) {
+		if (inputEl.checked) {
+			memo[inputEl.dataset.key] = inputEl.dataset.value;
+		}
+		return memo;
+	}, {});
 
-  chrome.storage.sync.set({
-    sites: sites
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-      window.close();
-    }, 800);
-  });
+	chrome.storage.sync.set({
+		sites: sites
+	}, function () {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function () {
+			status.textContent = '';
+			window.close();
+		}, 800);
+	});
 }
 
 // Restores checkbox input states using the preferences
 // stored in chrome.storage.
 function renderOptions() {
-  chrome.storage.sync.get({
-    sites: {}
-  }, function(items) {
-    let sites = items.sites;
-    let sitesEl = document.getElementById('bypass_sites');
-    for (const key in defaultSites) {
-      if (!defaultSites.hasOwnProperty(key)) {
-        continue;
-      }
+	chrome.storage.sync.get({
+		sites: {}
+	}, function (items) {
+		let sites = items.sites;
+		let sitesEl = document.getElementById('bypass_sites');
+		for (const key in defaultSites) {
+			if (!defaultSites.hasOwnProperty(key)) {
+				continue;
+			}
 
-      let value = defaultSites[key];
-      let labelEl = document.createElement('label');
-      let inputEl = document.createElement('input');
-      inputEl.type = 'checkbox';
-      inputEl.dataset.key = key;
-      inputEl.dataset.value = value;
-      inputEl.checked = key in sites;
+			let value = defaultSites[key];
+			let labelEl = document.createElement('label');
+			let inputEl = document.createElement('input');
+			inputEl.type = 'checkbox';
+			inputEl.dataset.key = key;
+			inputEl.dataset.value = value;
+			inputEl.checked = key in sites;
 
-      labelEl.appendChild(inputEl);
-      labelEl.appendChild(document.createTextNode(' '+key));
-      sitesEl.appendChild(labelEl);
-    }
-  });
+			labelEl.appendChild(inputEl);
+			labelEl.appendChild(document.createTextNode(' ' + key));
+			sitesEl.appendChild(labelEl);
+		}
+	});
 }
 
 function selectAll() {
-  let inputEls = Array.from(document.querySelectorAll('input'));
-  inputEls.forEach(function(inputEl) {
-    inputEl.checked = true;
-  });
+	let inputEls = Array.from(document.querySelectorAll('input'));
+	inputEls.forEach(function (inputEl) {
+		inputEl.checked = true;
+	});
 }
 
 function selectNone() {
-  let inputEls = Array.from(document.querySelectorAll('input'));
-  inputEls.forEach(function(inputEl) {
-    inputEl.checked = false;
-  });
+	let inputEls = Array.from(document.querySelectorAll('input'));
+	inputEls.forEach(function (inputEl) {
+		inputEl.checked = false;
+	});
 }
 
 document.addEventListener('DOMContentLoaded', renderOptions);
